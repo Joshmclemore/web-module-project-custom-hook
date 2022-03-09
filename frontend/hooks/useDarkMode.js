8a3e415dab8b22da2e1bea5d5f910a0ff64823bd
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-09 15:57:21 -0600

    moved styles to index.js and tests are passing now

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = (initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage("darkmode", initialValue)
+    const [darkMode, setDarkMode] = useLocalStorage("dark mode", initialValue)
     return [darkMode, setDarkMode];
 }
 
